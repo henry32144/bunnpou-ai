54f95196d1612e3140f033ddfc62aa6800e49038
Sky <chenghanwu32144@gmail.com>
2023-06-07 15:49:38 -0700

    Add library

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="flex">
+      <div className="md:container md:mx-auto py-24 prose">
+        <h1>Project title</h1>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">OPENAI API KEY</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <label className="label">
+            <span className="label-text">OPENAI API SECRET</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+      </div>
     </main>
   );
 }
